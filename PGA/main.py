50067bc654cc9641fc546772ceb1dfcb357e0ab0
Kerem <base@Kerems-MacBook-Pro.local>
2020-03-01 18:17:37 +0100

    pixel values are normalized

--- PGA/main.py
+++ PGA/main.py
@@ -5,24 +5,39 @@ import json
 import re
 from os.path import join
 from PIL import Image
+from torch import float32, as_tensor
 import torch
 import numpy as np
 from torch.utils.data import DataLoader, Dataset
+from skimage import transform
+from torchvision.transforms import transforms
 
-ms_images_path = '/Users/base/MEGA/Universität/Tez Calismasi/dl_data/BigEarth1000'
+class Normalize(object):
+    def __call__(self, sample):
+        sample -= sample.mean()
+        sample /= sample.std()
+        return sample
+
+class Rescale(object):
+    def __init__(self, output_shape):
+        self.output_shape = output_shape
+
+    def __call__(self, sample):
+        updated_sample = transform.resize(sample, self.output_shape)
+        return updated_sample
 
 class BigEarthDataSet(Dataset):
     def __init__(self, ms_images_path, transform=None):
         super(BigEarthDataSet).__init__()
         self.ms_images_path = ms_images_path
         self.transform = transform
+        self.ms_images_dirs = self._get_ms_image_dirs()
 
     def __len__(self):
         return len(self._get_ms_image_dirs())
 
     def __getitem__(self, idx):
-        ms_images_dirs = self._get_ms_image_dirs()
-        ms_image_dir_path = join(self.ms_images_path, ms_images_dirs[idx])
+        ms_image_dir_path = join(self.ms_images_path, self.ms_images_dirs[idx])
         ms_image_dir_content = shutil.os.listdir(ms_image_dir_path)
 
         metadata_index = self._get_index_of_metadata_file(ms_image_dir_content)
@@ -38,11 +53,12 @@ class BigEarthDataSet(Dataset):
         for img_path in sorted_ms_image_paths:
             cur_img_path = join(ms_image_dir_path, img_path)
             cur_img = Image.open(cur_img_path).getdata()
-            cur_img_array = np.asarray(cur_img)
-            cur_img_as_arrays.append(cur_img_array)
+            cur_img_array = as_tensor(cur_img, dtype=float32).view(cur_img.size)
+            cur_img_transformed = self.transform(cur_img_array) if self.transform else cur_img_array
+            cur_img_as_arrays.append(cur_img_transformed)
 
-        if self.transform:
-            cur_img_as_arrays = self.transform(cur_img_as_arrays)
+        h, w = cur_img_as_arrays[0].shape
+        cur_img_as_arrays = torch.as_tensor(cur_img_as_arrays).view((-1, h, w))
 
         return {'data': cur_img_as_arrays, 'labels': labels}
 
@@ -71,11 +87,18 @@ class BigEarthDataSet(Dataset):
         ms_images_with_band.sort(key=lambda elem: elem[0])
         sorted_images = [img[1] for img in ms_images_with_band]
         return sorted_images
-
-be_dataset = BigEarthDataSet(ms_images_path)
+        
+ms_images_path = '/Users/base/MEGA/Universität/Tez Calismasi/dl_data/BigEarth1000'
+# composed_transforms = transforms.Compose([Rescale((120, 120)), ToTensor() , Normalize([0.5], [0.5])])
+composed_transforms = transforms.Compose([Rescale((120, 120)), Normalize()])
+be_dataset = BigEarthDataSet(ms_images_path, composed_transforms)
 data = DataLoader(be_dataset)
 
 for index, sample_batch in enumerate(data):
     if index >= 50:
         break
     print(sample_batch)
+
+# TODO: Train, validation ve test olarak ayir verileri.
+
+
